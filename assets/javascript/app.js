f016c36b3bd359040ea6171a97947e1a348a85b9
mmusil2 <mmusil2@kent.edu>
2018-12-11 11:15:56 -0500

    app fully functional. added styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,70 +1,70 @@
-var config = {
-    apiKey: "AIzaSyDISUwVor5sk2snvk6GtBU0WpqvfPaKZWE",
-    authDomain: "train-scheduler-db01e.firebaseapp.com",
-    databaseURL: "https://train-scheduler-db01e.firebaseio.com",
-    projectId: "train-scheduler-db01e",
-    storageBucket: "train-scheduler-db01e.appspot.com",
-    messagingSenderId: "184769248717"
-  };
-  firebase.initializeApp(config);
+$(document).ready(function () {
 
-var database = firebase.database();
+    var config = {
+        apiKey: "AIzaSyDISUwVor5sk2snvk6GtBU0WpqvfPaKZWE",
+        authDomain: "train-scheduler-db01e.firebaseapp.com",
+        databaseURL: "https://train-scheduler-db01e.firebaseio.com",
+        projectId: "train-scheduler-db01e",
+        storageBucket: "train-scheduler-db01e.appspot.com",
+        messagingSenderId: "184769248717"
+    };
+    firebase.initializeApp(config);
 
-$("#submit").on("click", function(event) {
-    event.preventDefault();
-  
-    var name = $("#name").val().trim();
-    var dest = $("#dest").val().trim();
-    var start = $("#start").val().trim();
-    var freq = $("#freq").val().trim();
+    var database = firebase.database();
 
-    database.ref().push({
-        name: name,
-        dest: dest,
-        start: start,
-        freq: freq
+    $("#submit").on("click", function(event) {
+        event.preventDefault();
+    
+        var name = $("#name").val().trim();
+        var dest = $("#dest").val().trim();
+        var start = $("#start").val().trim();
+        var freq = $("#freq").val().trim();
+
+        database.ref().push({
+            name: name,
+            dest: dest,
+            start: start,
+            freq: freq,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
     });
+
+
+    database.ref().on("child_added", function(childSnapshot) {
+        console.log(childSnapshot.val().name);
+        console.log(childSnapshot.val().dest);
+        console.log(childSnapshot.val().start);
+        console.log(childSnapshot.val().freq);
+
+        var newName = childSnapshot.val().name;
+        var newDest = childSnapshot.val().dest;
+        var newStart = childSnapshot.val().start;
+        var newFreq = childSnapshot.val().freq;
+
+        // first train time (pushed back 1 year to make sure it comes before current time)
+        var startTimeConverted = moment(newStart, "hh:mm").subtract(1, "years");
+
+        var diffTime = moment().diff(moment(startTimeConverted), "minutes");
+
+        var timeRemainder = diffTime % newFreq;
+
+        var minutesAway = newFreq - timeRemainder;
+
+        var nextTrain = moment().add(minutesAway, "minutes");
+        var catchTrain = moment(nextTrain).format("HH:mm");
+
+        $("#all-trains").append(
+            '<tr><td>' + newName + 
+            '</td><td>' + newDest +
+            '</td><td>' + newFreq +
+            '</td><td>' + catchTrain +
+            '</td><td>' + minutesAway + '</td></tr>'
+        );
+
+        $("#name, #dest, #start, #freq").val("");
+        return false;
+    }, function (errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+    });
+
 });
-
-database.ref().orderByChild("dateAdded").limitToLast(100).on("child_added", function(snapshot) {
-    console.log(snapshot.val().name);
-    
-    var tRow = $("<tr>");
-    
-    var nameTd = $("<td>").text(snapshot.val().name);
-    var destTd = $("<td>").text(snapshot.val().dest);
-    var freqTd = $("<td>").text(snapshot.val().freq);
-    var nextTd = $("<td>").text(snapshot.val().next);
-    var minTd = $("<td>").text(snapshot.val().min);
-
-    tRow.append(nameTd, destTd, freqTd, nextTd, minTd);
-    
-    $("tbody").append(tRow);
-});
-
-function renderTrains(list) {
-    // add for loop
-
-    var tRow = $("<tr>");
-    
-    var nameTd = $("<td>").text(list.name);
-    var destTd = $("<td>").text(list.dest);
-    var freqTd = $("<td>").text(list.freq);
-    var nextTd = $("<td>").text(list.next);
-    var minTd = $("<td>").text(list.min);
-    
-    tRow.append(nameTd, destTd, freqTd, nextTd, minTd);
-    
-    $("tbody").append(tRow);
-}
-
-// list = {
-//     name: "train",
-//     dest: "NYC",
-//     freq: "30",
-//     next: "12:30 AM",
-//     min: "5"
-// }
-
-// renderTrains(list);
-
